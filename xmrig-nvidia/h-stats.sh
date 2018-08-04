97dab807be42e79c4eb00522776b072ea1af93be
Viktor Komarov <41443179+Kpaccyc@users.noreply.github.com>
2018-08-05 02:49:32 +0300

    Update h-stats.sh

--- xmrig-nvidia/h-stats.sh
+++ xmrig-nvidia/h-stats.sh
@@ -7,9 +7,6 @@ else
 khs=`echo $stats_raw | jq -r '.hashrate.total[0]' | awk '{print $1/1000}'`
 local ac=$(jq '.results.shares_good' <<< "$stats_raw")
 local rj=$(( $(jq '.results.shares_total' <<< "$stats_raw") - $ac ))
-local hs_units="hs"
-local hs=$(jq '.hashrate.threads' <<< "stats_raw")
-stats=$(jq --argjson temp "$temp" --argjson fan "$fan" --arg ac "$ac" --arg rj "$rj" --arg hs_units "$hs_units"  \
+stats=$(jq --argjson temp "$temp" --argjson fan "$fan" --arg ac "$ac" --arg rj "$rj" \
 '{hs: [.hashrate.threads[][0]], algo: .algo, $temp, $fan, uptime: .connection.uptime, ar: [$ac, $rj]}' <<< "$stats_raw")
 fi
-
